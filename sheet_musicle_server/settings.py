096549c6fbd654cd1f0b4c780052f4a5c51d2fe0
Reed Trevelyan <reedtrevelyan@gmail.com>
2024-03-02 19:58:39 -0500

    try this

--- sheet_musicle_server/settings.py
+++ sheet_musicle_server/settings.py
@@ -198,6 +198,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+if ENV == "prod":
+    STATIC_URL = "https://sheet-musicle.com/staticroot/"
+
 STATIC_ROOT = BASE_DIR / "staticroot"
 
 # Default primary key field type
